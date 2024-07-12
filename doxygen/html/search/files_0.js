4a9522881c7708731c68f2a450b37308202b334c
Kev1606 <kevincruz1606@estudiantec.cr>
2024-07-12 02:51:00 -0400

    Deployed bf247f1 with MkDocs version: 1.6.0

---
+++ doxygen/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['adis_2ecpp_0',['Adis.cpp',['../Adis_8cpp.html',1,'']]]
+];
---
+++ doxygen/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['main_0',['main',['../Adis_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;Adis.cpp'],['../paralelizar_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;paralelizar.cpp']]]
+];
